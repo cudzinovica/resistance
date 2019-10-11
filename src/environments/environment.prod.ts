d1dd32d90ee1d7dbe18bceee3f0d70b749db8b17
Anthony Cudzinovic <cudzinovica@gmail.com>
2019-10-11 15:45:34 -0600

    update production server uri to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  resistanceApiUri: 'http://resistance-game-api.herokuapp.com'
+  resistanceApiUri: 'https://resistance-game-api.herokuapp.com'
 };
