2bcb7ba82d7e62c9961ea5e7096c49352de39e71
Anthony Cudzinovic <cudzinovica@gmail.com>
2019-11-22 17:12:28 -0700

    turn off socket autoconnect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { GameService } from './services/game.service';
 
 @Component({
@@ -6,14 +6,10 @@ import { GameService } from './services/game.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'resistance';
 
   constructor(
     private gameService: GameService,
   ) {}
-
-  ngOnInit() {
-    this.gameService.disconnect();
-  }
 }
